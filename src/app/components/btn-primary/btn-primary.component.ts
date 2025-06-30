7e60afd1ce799258cba111bb015edd1eaeb62632
Pedro Silva <pedro.mape7@gmail.com>
2025-06-30 00:32:59 -0300

    feat: add component btn

---
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+type BtnVariant = "primary" | "secondary";
+
+@Component({
+  selector: 'app-btn-primary',
+  imports: [],
+  templateUrl: './btn-primary.component.html',
+  styleUrl: './btn-primary.component.scss'
+})
+export class BtnPrimaryComponent {
+  @Input("btn-text") btnText: String = "";
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() variant: BtnVariant = "primary";
+  @Output("submit") onSubmit = new EventEmitter();
+
+
+  public submit() {
+    this.onSubmit.emit();
+  }
+}
