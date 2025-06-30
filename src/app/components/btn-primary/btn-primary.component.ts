f04a2e3e8cf3c84dc1ea4fe369296f3f95013b17
Pedro Silva <pedro.mape7@gmail.com>
2025-06-30 11:20:28 -0300

    feat: começando o formulário

--- src/app/components/btn-primary/btn-primary.component.ts
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 type BtnVariant = "primary" | "secondary";
 
 @Component({
-  selector: 'app-btn-primary',
+  selector: 'btn-primary',
   imports: [],
   templateUrl: './btn-primary.component.html',
   styleUrl: './btn-primary.component.scss'
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [NgOptimizedImage, HeaderComponent, BtnPrimaryComponent, NewsletterFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  log(a: string): void {
+    console.log(a);
+  }
 }
