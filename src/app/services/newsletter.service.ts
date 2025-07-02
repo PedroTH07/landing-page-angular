733ba3513c84aa082c5464de61b9c53c9849dfd8
Pedro Silva <pedro.mape7@gmail.com>
2025-07-02 18:57:18 -0300

    fix: adicionado uma URL de uma API mockada

--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -3,18 +3,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 interface NewsletterResponse {
-    message: string
+    id: number;
+    data: {
+      name: string;
+      email: string;
+    };
 }
 
 @Injectable({ providedIn: 'root' })
 export class NewsletterService {
-  private endpointUrl: string = "https://util.devi.tools/";
+  private endpointUrl: string = "https://test-request-vin5.onrender.com/";
 
   constructor(private httpClient: HttpClient) { }
 
   sendData(name: string, email: string): Observable<NewsletterResponse> {
     const data = { name, email };
 
-    return this.httpClient.post<NewsletterResponse>(this.endpointUrl + "api/v1/notify", data);
+    return this.httpClient.post<NewsletterResponse>(this.endpointUrl + "api", data);
   }
 }
